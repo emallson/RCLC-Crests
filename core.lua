cba3a1286c6c978774ac083dac852eb747891e19
J. David Smith <emallson@archlinux.us>
2025-07-26 11:22:53 -0400

    first raid test

--- core.lua
+++ core.lua
@@ -5,8 +5,21 @@ local module = rclc:NewModule('RCLC Crests', 'AceComm-3.0', 'AceSerializer-3.0')
 local coreVotingFrame = rclc:GetModule('RCVotingFrame')
 local votingFrame = module:NewModule('RCLC Crests - Voting Frame', 'AceTimer-3.0', 'AceEvent-3.0')
 
+---@class ItemData
+---@field item string
+---@field equipped? boolean|nil
+---@field offspec? boolean|nil
+
+---@class BagData
+---@field items ItemData[]
+---@field highWatermark number
+
+---@type number|nil
 local session = nil
+---memory cache of player name -> slot id -> bag data
+---@type table<string, table<number, BagData>>
 local PLAYER_SLOT_CACHE = {}
+
 function votingFrame:OnMessageReceived(msg, ...)
     if msg == "RCSessionChangedPre" then
         local s = unpack({ ... })
@@ -15,6 +28,10 @@ function votingFrame:OnMessageReceived(msg, ...)
     end
 end
 
+---get the track label and track id from a tier token bonus id. nil, nil if the item does not have any known bonus id
+---@param item string
+---@return string|nil
+---@return integer|nil
 local function trackFromTokenBonusId(item)
     if string.find(item, ':10355:') then
         return 'Hero', 974
@@ -40,6 +57,22 @@ local TRACKS = {
     [972] = 645,
 }
 
+local TRACK_DISPLAY_QUALITY = {
+    [978] = 4,
+    [974] = 3,
+    [973] = 2,
+    [972] = 1,
+}
+
+local function formatTrackName(trackId, trackName)
+    local quality = TRACK_DISPLAY_QUALITY[trackId] or 0
+
+    return '|cnIQ' .. quality .. ':' .. trackName .. '|r'
+end
+
+---check if the upgrade info represents a current-tier track. the track ids remain the same across seasons, but the ilvls do not
+---@param upgradeInfo unknown|nil
+---@return boolean
 local function isCurrentTrack(upgradeInfo)
     if not upgradeInfo then return false end
     return TRACKS[upgradeInfo.trackStringID] == upgradeInfo.maxItemLevel
@@ -48,6 +81,9 @@ end
 -- bonus id for current season crafted items
 local CURRENT_CRAFT_BONUS = 12040
 
+---check if the item link represents a current-tier spark craft
+---@param itemLink string
+---@return boolean
 local function isCurrentCrafted(itemLink)
     return string.find(itemLink, ':' .. CURRENT_CRAFT_BONUS .. ':') ~= nil
 end
@@ -86,7 +122,9 @@ local CURRENT_TIER_TOKENS = {
     [228819] = "MultiSlots"
 }
 
--- get the redundancy slot id(s) for the item. this may return multiple (e.g. for omnitokens)
+---get the redundancy slot id(s) for the item. this may return multiple (e.g. for omnitokens)
+---@param itemLink string
+---@return number[]
 local function tierTokenRedundancySlots(itemLink)
     local id = C_Item.GetItemIDForItemInfo(itemLink)
     local slot = CURRENT_TIER_TOKENS[id] or RCTokenTable[id] or rclc:GetTokenSlotFromTooltip(id)
@@ -100,15 +138,20 @@ local function tierTokenRedundancySlots(itemLink)
     return {}
 end
 
+---check if the item link is a current-tier token
+---@param itemLink string
+---@return boolean
 local function isCurrentTierToken(itemLink)
     return CURRENT_TIER_TOKENS[C_Item.GetItemIDForItemInfo(itemLink)] ~= nil
 end
 
+---iterator over bag contents
 local function iterBags()
     local currentBag = 0
     local maxBag = 4 -- ignoring reagent bag
     local currentSlot = 0
     local maxBagSlots = C_Container.GetContainerNumSlots(currentBag)
+    ---@return string|nil, number|nil, number|nil
     return function()
         while true do
             currentSlot = currentSlot + 1
@@ -119,7 +162,7 @@ local function iterBags()
             end
 
             if currentBag > maxBag then
-                return
+                return nil
             end
 
             local link = C_Container.GetContainerItemLink(currentBag, currentSlot)
@@ -153,12 +196,48 @@ local function matchesRedundancySlot_(left, right)
     end
 end
 
+---check if two redundancy slots match. this handles the relationship between onehand / mainhand / offhand / twohand weapons (i hope)
+---@param left number
+---@param right number
+---@return boolean
 local function matchesRedundancySlot(left, right)
     return matchesRedundancySlot_(left, right) or matchesRedundancySlot_(right, left)
 end
 
+---check if the item is usable by the current player class. some items do not have tags for this, and we assume they are usable
+---@param item string
+---@return boolean
+local function isUsableByClass(item)
+    local info = C_Item.GetItemSpecInfo(item)
+
+    return info == nil or #info > 0
+end
+
+---check if the item is intended for the current player spec. some items do not have tags for this, and we assume they are. "intended" means things like dps trinkets aren't flagged for tank specs. this is informative because fuck tank trinkets
+---@param item string
+---@return boolean
+local function isIntendedForCurrentSpec(item)
+    local info = C_Item.GetItemSpecInfo(item)
+
+    if info == nil then
+        return true -- default assume yes
+    end
+
+    local currentSpecId = GetSpecializationInfo(GetSpecialization())
+    for _, specId in ipairs(info) do
+        if specId == currentSpecId then
+            return true
+        end
+    end
+
+    return false
+end
+
 -- TODO: should redo this to do one scan and one message to the channel, though i suppose could also argue that any autopassers shouldn't get requested
+---@param redundancySlotId number
+---@return ItemData[]
 local function listSlotItems(redundancySlotId)
+    ---@type ItemData[]
     local result = {}
 
     for i = 1, 16 do
@@ -169,14 +248,10 @@ local function listSlotItems(redundancySlotId)
             if equipped then
                 local red = C_ItemUpgrade.GetHighWatermarkSlotForItem(equipped)
                 if matchesRedundancySlot(red, redundancySlotId) then
-                    table.insert(result, equipped)
+                    table.insert(result, { item = equipped, equipped = true })
                 end
             end
         end
-    end
-
-    if equipped then
-        table.insert(result, equipped)
     end
 
     for item, bag, slot in iterBags() do
@@ -191,15 +266,21 @@ local function listSlotItems(redundancySlotId)
                 end
             end
         end
-        if matchesRedundancySlot(red, redundancySlotId) then
+        if matchesRedundancySlot(red, redundancySlotId) and isUsableByClass(item) then
             if isCurrentTrack(upgradeInfo) or isCurrentCrafted(item) or isCurrentTierToken(item) then
-                table.insert(result, item)
+                table.insert(result, {
+                    item = item,
+                    offspec = not isIntendedForCurrentSpec(item)
+                })
             end
         end
     end
     return result
 end
 
+---collect slot data from the current player
+---@param redundancySlotId number
+---@return BagData
 local function slotData(redundancySlotId)
     local items = listSlotItems(redundancySlotId)
 
@@ -209,17 +290,19 @@ local function slotData(redundancySlotId)
     }
 end
 
+---@param player string
+---@param redundancySlotId number
 local function requestPlayerSlotData(player, redundancySlotId)
     local targetName = Ambiguate(player, 'mail')
     if PLAYER_SLOT_CACHE[targetName] and PLAYER_SLOT_CACHE[targetName][redundancySlotId] then
         return -- already have the data
     end
 
-    -- local name, server = UnitFullName('player')
-    -- if name .. '-' .. server == targetName then
-    --     votingFrame:SendMessage('RCLCCrestUpdatePlayer', targetName, redundancySlotId, slotData(redundancySlotId))
-    --     return -- do nothing for self
-    -- end
+    local name, server = UnitFullName('player')
+    if name .. '-' .. server == targetName then
+        votingFrame:SendMessage('RCLCCrestUpdatePlayer', targetName, redundancySlotId, slotData(redundancySlotId))
+        return -- do nothing for self
+    end
 
     module:RequestSlot(redundancySlotId, targetName)
 end
@@ -248,25 +331,70 @@ local function Tooltip_LabelledLine(leftText, rightText)
     tipFrame:AddDoubleLine(leftText, rightText)
 end
 
+---determine the highest track from *main-spec* items in the list
+---@param items ItemData[]
+---@return string|nil
+---@return integer|nil
 local function highestTrackFromItems(items)
     local track = nil
     local trackIlvl = nil
-    for _, item in pairs(items) do
-        local upgradeInfo = C_Item.GetItemUpgradeInfo(item)
-        if upgradeInfo and (not trackIlvl or upgradeInfo.maxItemLevel > trackIlvl) then
-            track = upgradeInfo.trackString
-            trackIlvl = upgradeInfo.maxItemLevel
-        elseif isCurrentTierToken(item) then
-            local trackName, trackId = trackFromTokenBonusId(item)
+    for _, itemData in pairs(items) do
+        if not itemData.offspec then
+            local item = itemData.item
+            local upgradeInfo = C_Item.GetItemUpgradeInfo(item)
+            if upgradeInfo and (not trackIlvl or upgradeInfo.maxItemLevel > trackIlvl) then
+                track = upgradeInfo.trackString
+                trackIlvl = upgradeInfo.maxItemLevel
+            elseif isCurrentTierToken(item) then
+                local trackName, trackId = trackFromTokenBonusId(item)
 
-            if TRACKS[trackId] and (not trackIlvl or TRACKS[trackId] > trackIlvl) then
-                track = trackName
-                trackIlvl = TRACKS[trackId]
+                if TRACKS[trackId] and (not trackIlvl or TRACKS[trackId] > trackIlvl) then
+                    track = trackName
+                    trackIlvl = TRACKS[trackId]
+                end
             end
         end
     end
 
     return track, trackIlvl
+end
+
+---comparator for sorting item data. sort is: equipped desc, offspec asc, max ilvl desc, current ilvl desc
+---@param a ItemData
+---@param b ItemData
+---@return boolean
+local function compareItemData(a, b)
+    if b.equipped and not a.equipped then
+        return false
+    end
+    if a.equipped and not b.equipped then
+        return true
+    end
+
+    if b.offspec and not a.offspec then
+        return true --note: flipped
+    end
+    if a.offspec and not b.offspec then
+        return false
+    end
+
+    local up_a = C_Item.GetItemUpgradeInfo(a.item)
+    local up_b = C_Item.GetItemUpgradeInfo(b.item)
+
+    if up_b and up_a then
+        if up_b.maxItemLevel ~= up_a.maxItemLevel then
+            return up_b.maxItemLevel < up_a.maxItemLevel
+        end
+    end
+
+    local ilvl_a = C_Item.GetDetailedItemLevelInfo(a.item)
+    local ilvl_b = C_Item.GetDetailedItemLevelInfo(b.item)
+
+    if ilvl_a and ilvl_b then
+        return ilvl_b < ilvl_a
+    end
+
+    return ilvl_a ~= nil
 end
 
 local function trackUpgrade(self, frame, data, cols, row, realrow, column, fShow, ...)
@@ -322,7 +450,7 @@ local function trackUpgrade(self, frame, data, cols, row, realrow, column, fShow
                 Tooltip_Line(targetName)
                 for slotId, data in pairs(slotData) do
                     if #data.items > 0 then
-                        local slot = select(9, C_Item.GetItemInfo(data.items[1]))
+                        local slot = select(9, C_Item.GetItemInfo(data.items[1].item))
                         Tooltip_LabelledLine(_G[slot],
                             string.format('|Tinterface/icons/inv_valorstone_base:0:0:2:0|t to %d', data.highWatermark))
 
@@ -330,7 +458,10 @@ local function trackUpgrade(self, frame, data, cols, row, realrow, column, fShow
                             string.format('%8s | %4s | %4s | %5s | %5s', 'Track', 'ilvl', 'max', 'stats', 'tert'))
                         tipFrame:AddLine()
 
-                        for _, item in pairs(data.items) do
+                        table.sort(data.items, compareItemData)
+
+                        for _, itemData in pairs(data.items) do
+                            local item = itemData.item
                             local upgrade = C_Item.GetItemUpgradeInfo(item)
                             local ilvl = C_Item.GetDetailedItemLevelInfo(item)
                             local stats = C_Item.GetItemStats(item)
@@ -361,16 +492,26 @@ local function trackUpgrade(self, frame, data, cols, row, realrow, column, fShow
                                 local trackName_, trackId = trackFromTokenBonusId(item)
 
                                 if TRACKS[trackId] then
-                                    trackName = trackName_
+                                    trackName = formatTrackName(trackId, trackName_)
                                     trackIlvl = TRACKS[trackId]
                                 end
                             elseif upgrade and upgrade.trackString then
-                                trackName = upgrade.trackString
+                                trackName = formatTrackName(upgrade.trackStringID, upgrade.trackString)
                                 trackIlvl = upgrade.maxItemLevel
                             end
 
+                            local leftText = item
+
+                            if itemData.equipped then
+                                leftText = leftText .. ' (Eq)'
+                            end
+
+                            if itemData.offspec then
+                                leftText = leftText .. ' (OS)'
+                            end
+
                             Tooltip_LabelledLine(
-                                item,
+                                leftText,
                                 string.format('%8s | %4s | %4s | %5s | %5s',
                                     trackName,
                                     ilvl,
@@ -391,7 +532,7 @@ local function trackUpgrade(self, frame, data, cols, row, realrow, column, fShow
     end
 end
 
-local MSG_FORMAT_VERSION = 1
+local MSG_FORMAT_VERSION = 2
 
 function votingFrame:OnInitialize()
     -- parts of this are cribbed from the wowaudit plugin
@@ -431,7 +572,7 @@ function module:OnCommReceived(prefix, message, distribution, sender)
                     player = string.join("-", UnitFullName('player')),
                     type = 'request-slot-reply'
                 }
-    
+
                 module:SendCommMessage('RCLCCrests', self:Serialize(reply), 'RAID')
             end)
         elseif data.type == 'request-slot-reply' then
